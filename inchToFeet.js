c68ac2ece79aac6c21de0760d8de0ff327545e69
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-10-03 07:16:10 +0800

    Started Apply JavaScript Concepts

---
+++ inchToFeet.js
@@ -0,0 +1,12 @@
+//inch to feet
+
+function inchToFeet(inch){
+    var feet = inch / 12;
+    return feet;
+}
+
+var nanaFeet = inchToFeet(120);
+console.log(nanaFeet);
+
+var naniFeet = inchToFeet((300));
+console.log(naniFeet);

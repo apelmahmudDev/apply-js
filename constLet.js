58b64f9f8c05bb8897214e3d284a6062193b61e9
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-10-03 07:33:52 +0800

    Added const and let declairation method

---
+++ constLet.js
@@ -0,0 +1,15 @@
+let name = "Ab"
+
+if(name.length < 4) {
+    name = 'Abu';
+}
+
+console.log(name)
+
+let age = 15;
+age = 30;
+age = 33;
+console.log(age)
+
+const country = 'Bangladesh';
+console.log(country)
